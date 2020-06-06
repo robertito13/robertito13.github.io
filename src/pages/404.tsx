3f5bc2f5cce5f18f4c2951c75f321296a7524e82
Roberto Vaccaro <roberto.vaccaro@gmail.com>
2020-06-06 13:10:48 -0300

    Total Reboot

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+  <main>
+    <p>404</p>
+  </main>
+);
+
+export default NotFound;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
 
-import SEO from '../components/seo';
-import Head from '../components/head';
-import Articles from '../components/articles';
+const Main = () => (
+  <main>
+    <p>rvaccaro.com.ar</p>
+  </main>
+);
 
-import '../styles/site.scss';
-
-const BlogIndex = () => {
-  return (
-    <React.Fragment>
-      <SEO />
-      <Head />
-      <Articles />
-    </React.Fragment>
-  );
-};
-
-export default BlogIndex;
+export default Main;
