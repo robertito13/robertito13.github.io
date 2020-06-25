d97c08c0c025cd86cd3b50a85e7d607b275d9441
Roberto Vaccaro <roberto.vaccaro@gmail.com>
2020-06-25 12:01:38 -0300

    Formato artículos

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
+import Sidebar from '../components/sidebar';
+
+import styles from '../styles/content.module.scss';
+
 const BlogPostTemplate = ({ data, pageContext, location }) => {
   const post = data.markdownRemark;
 
   return (
-    <article>
-      <header>
-        <h1>{post.frontmatter.title}</h1>
-        <p>{post.frontmatter.date}</p>
-      </header>
-      <section dangerouslySetInnerHTML={{ __html: post.html }} />
-    </article>
+    <div className="container">
+      <Sidebar />
+      <article className={styles.content}>
+        <header>
+          <h1>{post.frontmatter.title}</h1>
+          <p>{post.frontmatter.date}</p>
+        </header>
+        <section dangerouslySetInnerHTML={{ __html: post.html }} />
+      </article>
+    </div>
   );
 };
 
